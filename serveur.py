a546c22839b22a1415714a5e782dafdd6d5bc17e
Ludovic Bouguerra <ludovic.bouguerra@kalyzee.com>
2019-12-20 10:49:08 +0100

    Ajout de la fonction multi

--- serveur.py
+++ serveur.py
@@ -28,3 +28,17 @@ def sub(val1, val2):
     """
     return val1 - val2
 
+def mul(val1, val2):
+    """Multiplication valeurs entre elles
+
+    @type val1: int | float
+    @param val1: Valeur 1
+
+    @type val2: int | float
+    @param val2: Valeur 2
+
+    @rtype: int | float
+    @return: Retourne la multiplication des deux valeurs
+    """
+    return val1 * val2
+
--- tests.py
+++ tests.py
@@ -8,3 +8,8 @@ assert(serveur.sub(2, 1) == 1)
 assert(serveur.sub(3, 1) == 2)
 assert(serveur.sub(5, 1) == 4)
 assert(serveur.sub(1, 5) == -4)
+
+assert(serveur.mul(1, 2) == 2)
+assert(serveur.mul(3, 1) == 3)
+assert(serveur.mul(5, 2) == 10)
+assert(serveur.mul(7, -5) == -35)
